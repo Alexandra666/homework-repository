3e36e75067d4882f774cf8349637ee76588a1962
Alexandra666 <georgievaalexandra95@gmail.com>
2021-04-22 20:35:27 +0300

    test3 passed

--- tests/homework1/test_task05.py
+++ tests/homework1/test_task05.py
@@ -14,4 +14,4 @@ def test_all_negative_case():
 
 def test_pos_neg_case():
     """Testing that returns correct with positive and negative integers"""
-    assert find_maximal_subarray_sum([[1, 3, -1, -3, 5, 3, 6, 7]], 3) == 16
+    assert find_maximal_subarray_sum([1, 3, -1, -3, 5, 3, 6, 7], 3) == 16
