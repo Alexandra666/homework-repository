63b2e8576e01924d1ab6d3137e9316447b715627
Alexandra666 <georgievaalexandra95@gmail.com>
2021-05-20 19:22:51 +0300

    add sphinx-type docstrings

--- homework5/oop_1.py
+++ homework5/oop_1.py
@@ -41,35 +41,88 @@ import datetime
 
 
 class Homework:
-    def __init__(self, text: str, deadline: int):
+    """This is a class which contains homework description
+        and the number of days given to complete the task
+        with checking expiration of deadline.
+
+
+    :param text: Description of homework
+    :type text: str
+    :param deadline_days: The number of days given to complete the task
+    :type deadline_days: int
+    """
+
+    def __init__(self, text: str, deadline_days: int):
+        """Constructor method"""
         self.text = text
-        self.deadline = datetime.timedelta(days=deadline)
+        self.deadline_days = datetime.timedelta(days=deadline_days)
         self.created = datetime.datetime.now()
 
     def is_active(self) -> bool:
-        """Check whether homework deadline is active or expired"""
-        return self.created + self.deadline > datetime.datetime.now()
+        """This function checks whether homework deadline is active or expired
+
+        :return: `True` if homework is active (deadline not expired) `False` otherwise
+        :rtype: bool
+        """
+        return self.created + self.deadline_days > datetime.datetime.now()
 
 
 class Student:
+    """This is a class which contains full name of a student
+        and allows to check deadline expiration of some homework.
+
+
+    :param first_name: First name of a student
+    :type first_name: str
+    :param last_name: Last name of a student
+    :type last_name: str
+    """
+
     def __init__(self, first_name: str, last_name: str):
+        """Constructor method"""
         self.last_name = last_name
         self.first_name = first_name
 
     @staticmethod
-    def do_homework(homework: Homework) -> Homework or None:
-        """Returns homework expired or not"""
+    def do_homework(homework: Homework) -> Homework:
+        """This function returns homework expired or not.
+
+        :param homework: object with homework description
+            and number of days given to complete the task
+        :type homework: Homework
+        :return: homework if homework is active and None otherwise.
+            In the latter case prints 'You are late!'
+        :rtype: Homework
+        """
         if homework.is_active():
             return homework
         print("You are late")
 
 
 class Teacher:
+    """This is a class which contains full name of a teacher
+        and allows to create some homework.
+
+
+    :param first_name: First name of a teacher
+    :type first_name: str
+    :param last_name: Last name of a teacher
+    :type last_name: str
+    """
+
     def __init__(self, first_name: str, last_name: str):
         self.last_name = last_name
         self.first_name = first_name
 
     @staticmethod
-    def create_homework(text: str, deadline: int) -> Homework:
-        """Create homework with text"""
-        return Homework(text, deadline)
+    def create_homework(text: str, deadline_days: int) -> Homework:
+        """This function creates homework with text and deadline days given
+
+        :param text: Description of the homework
+        :type text: str
+        :param deadline_days: The number of days given to complete the task
+        :type deadline_days: int
+        :return: homework
+        :rtype: Homework
+        """
+        return Homework(text, deadline_days)
